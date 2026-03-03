72c5179379de3b941ce2df0e14e2841495adf860
renatinha-cegoni <renatinha.cegoni@gmail.com>
2026-03-03 02:54:50 +0000

    fix: remover duplicações e usar process.env.PORT

--- server/index.ts
+++ server/index.ts
@@ -89,7 +89,8 @@ app.use((req, res, next) => {
   // Other ports are firewalled. Default to 5000 if not specified.
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = parseInt(process.env.PORT || "5000", 10);
+  const port = parseInt(process.env.PORT || "3000", 10);
+
   httpServer.listen(
     {
       port,
@@ -97,23 +98,6 @@ app.use((req, res, next) => {
       reusePort: true,
     },
     () => {
-      log(`serving on port ${port}`);
-    },
+      console.log(`Server running on port ${port}`);
+    }
   );
-})();
-const port = parseInt(process.env.PORT || "5000", 10);
-httpServer.listen(
-    {
-        port,
-        host: "0.0.0.0",
-        reusePort: true,
-    },
-    () => {
-        log(`serving on port ${port}`);
-    },
-);
-const PORT = process.env.PORT || 3000;
-
-httpServer.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
